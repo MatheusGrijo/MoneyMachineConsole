73e59d4cbda1c2c51829554ec3b99da01a0bb0b5
MatheusGrijo <matheusgrijo@matheusgrijo.com.br>
2018-01-01 22:17:12 -0200

    Initial commit

---
+++ Key.cs
@@ -0,0 +1,15 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: mifus_000
+ * Date: 20/05/2017
+ * Time: 12:06
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+public class Key
+{
+	public static double fee = 0.15;
+	public static double profit = 0.01;
+	public static string key = "TCI2BAHG-CTL4XE9P-1C3JKIOD-J7DZHY01";
+	public static string secret = "5e0474707665d20b5cd50d8a6282b7cf1064321e58f0296cb883a670708fed8c74b1d8fa3781151c5f80f9cdf4d1fc4a44037a138c7baead97ac17bfa580b079";
+}
---
+++ bin/Debug/db.io
@@ -0,0 +1,9 @@
+<?xml version="1.0"?>
+<MyOperation xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+  <orderNumber>81462041465</orderNumber>
+  <typeOrder>buy</typeOrder>
+  <currencyPair>BTC_LTC</currencyPair>
+  <amount>0.01</amount>
+  <rate>0.01250000</rate>
+  <date>2017-05-21T22:02:21.8920212-03:00</date>
+</MyOperation>
